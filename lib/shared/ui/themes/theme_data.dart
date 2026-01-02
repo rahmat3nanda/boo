29319aa2017ca20a0d77d14527a8dd710e3a14e4
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-02 23:57:39 +0700

    feat(ui): Create `BooThemeData` for application theming
    This commit introduces a new file, `lib/shared/ui/themes/theme_data.dart`, which defines the `ThemeData` for the application based on a custom `BooTheme`.
    
    Specific changes include:
    - A `booThemeData` function that returns a configured `ThemeData`.
    - The `Inter` font from `google_fonts` is set as the default text theme.
    - The color scheme is generated from `theme.color.turquoise1`.
    - `scaffoldBackgroundColor` is set from `theme.color.scaffold`.
    - The `AppBarTheme` is styled with a transparent background and `Inter` for its title.
    - `TabBarTheme` labels are also styled using the `Inter` font.
    - The default back button in the `ActionIconTheme` is replaced with a custom `Icons.arrow_back_ios` icon, styled to match the theme.

---
+++ lib/shared/ui/themes/theme_data.dart
@@ -0,0 +1,37 @@
+import 'package:boo/shared/ui/themes/theme.dart' show BooTheme;
+import 'package:flutter/material.dart'
+    show
+        ActionIconThemeData,
+        AppBarTheme,
+        ColorScheme,
+        Icon,
+        Icons,
+        TabBarThemeData,
+        ThemeData;
+import 'package:google_fonts/google_fonts.dart' show GoogleFonts;
+
+ThemeData _themeData(BooTheme theme) => ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: theme.color.turquoise1.value),
+  actionIconTheme: ActionIconThemeData(
+    backButtonIconBuilder: (_) => Icon(
+      Icons.arrow_back_ios,
+      color: theme.color.turquoise1.value,
+      size: 22,
+    ),
+  ),
+  scaffoldBackgroundColor: theme.color.scaffold.value,
+  appBarTheme: AppBarTheme(
+    titleTextStyle: GoogleFonts.inter(),
+    backgroundColor: theme.color.scaffold.value,
+    surfaceTintColor: theme.color.scaffold.value,
+  ),
+  tabBarTheme: TabBarThemeData(
+    labelStyle: GoogleFonts.inter(),
+    unselectedLabelStyle: GoogleFonts.inter(),
+  ),
+  textTheme: GoogleFonts.interTextTheme(),
+);
+
+ThemeData booThemeData(BooTheme theme) => _themeData(
+  theme,
+).copyWith(textTheme: GoogleFonts.interTextTheme(_themeData(theme).textTheme));
