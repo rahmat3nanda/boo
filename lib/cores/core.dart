f5375b7fcc0c2560c290e4d6128209b98a357122
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-03 16:25:14 +0700

    feat(core): Introduce BooCore service for initialization
    This commit introduces a new core service class, `BooCore`, responsible for centralizing application initialization logic.
    
    `BooCore` extends `BooService` and is designed as a singleton, accessible via `BooCore.i`.
    
    Specific changes include:
    - A `BooCore` class that acts as a central service.
    - A static `initialize` method that handles the setup process.
    - The initialization logic ensures `BooCore` is only registered once with `Get` and also calls `BooLogger.initialize()`.

---
+++ lib/cores/core.dart
@@ -0,0 +1,21 @@
+import 'package:boo/cores/logger.dart' show BooLogger;
+import 'package:boo/cores/service.dart' show BooService;
+import 'package:get/get.dart' show Get, Inst;
+import 'package:meta/meta.dart' show protected;
+
+@protected
+class BooCore extends BooService {
+  static BooCore get i => Get.find<BooCore>();
+
+  static Future<void> initialize() async {
+    try {
+      if (Get.isRegistered<BooCore>()) {
+        return;
+      }
+      BooLogger.initialize();
+      Get.put<BooCore>(BooCore());
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
