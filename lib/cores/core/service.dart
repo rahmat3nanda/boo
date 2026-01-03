1e4ce1443db2c00f30ddddfd3e05903435d90e5d
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-03 16:57:15 +0700

    refactor(core): Correct import path for BooLogger
    This commit adjusts the import path for `BooLogger` within the `CoreService` class.
    
    The path has been corrected from `package:boo/cores/logger.dart` to `package:boo/cores/core/logger.dart` to reflect its new location within the `core` directory.

--- lib/cores/core/service.dart
+++ lib/cores/core/service.dart
@@ -1,4 +1,4 @@
-import 'package:boo/cores/logger.dart' show BooLogger;
+import 'package:boo/cores/core/logger.dart' show BooLogger;
 import 'package:get/get.dart' show GetxService;
 import 'package:meta/meta.dart' show protected;
 
