ae95c21c70bc82226e6c378d10675655c2cdf2fe
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-03 17:15:48 +0700

    refactor(main): Convert MainPage to a routeable GetPage
    This commit refactors the `MainPage` to integrate with the `GetX` routing system.
    
    It replaces the previous `Placeholder` widget with a `BooScaffold`, establishing a basic page structure. A static getter, `page`, has been added to define the `GetPage` configuration, associating the `MainPage` widget with the `AppRoute.main` route.

--- lib/app/main/main.page.dart
+++ lib/app/main/main.page.dart
@@ -1,9 +1,14 @@
-import 'package:flutter/material.dart'
-    show Placeholder, StatelessWidget, Widget;
+import 'package:boo/app/app.routes.dart' show AppRoute;
+import 'package:boo/shared/ui/widgets/widgets.dart' show BooScaffold;
+import 'package:flutter/material.dart' show StatelessWidget, Widget;
+import 'package:get/get.dart' show GetPage;
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
+  static GetPage<dynamic> get page =>
+      GetPage<dynamic>(name: AppRoute.main.go, page: MainPage.new);
+
   @override
-  Widget build(_) => const Placeholder();
+  Widget build(_) => const BooScaffold();
 }
