a7d27236147a00c24f7e2c45f11c326bf1bf7cc1
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-03 20:24:42 +0700

    feat(ui): Add `bAppBar` constant for app bar height
    This commit introduces a new constant, `bAppBar`, to calculate the total height of the app bar.
    
    The constant is defined in a new file, `lib/shared/utils/const.dart`. It calculates the height by summing Flutter's `kToolbarHeight` with the top padding of the screen (i.e., the status bar height), which is retrieved using `Get.mediaQuery.padding.top`. This provides a reliable value for layout calculations involving the app bar.

---
+++ lib/shared/utils/const.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart' show kToolbarHeight;
+import 'package:get/get.dart' show Get, GetNavigation;
+
+final double bAppBar = kToolbarHeight + Get.mediaQuery.padding.top;
