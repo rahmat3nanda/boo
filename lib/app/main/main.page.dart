c94041ce4ab9422868986f156f33ee4de20d3a24
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-03 16:21:51 +0700

    refactor: Replace boilerplate counter with MainPage
    This commit removes the default Flutter counter application and replaces it with a new `MainPage`.
    
    The `MyHomePage` StatefulWidget, along with its state and counter logic, has been deleted from `lib/app/app.dart`. The `home` property of the `MaterialApp` now points to the new `MainPage`.
    
    A new `MainPage` stateless widget has been created in `lib/app/main/main.page.dart`, which currently returns a `Placeholder` widget.

---
+++ lib/app/main/main.page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart'
+    show Placeholder, StatelessWidget, Widget;
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(_) => const Placeholder();
+}
