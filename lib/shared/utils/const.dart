d736d0ab6f243e97918c04e37e01feca61739ff7
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-04 17:00:34 +0700

    feat(ui): Add constant for navigation bar height
    This commit introduces a new constant, `bNavBar`, to calculate the total height of the bottom navigation bar, including the safe area padding at the bottom of the screen.
    
    The `kBottomNavigationBarHeight` constant from Flutter's material library is now imported to support this calculation.

--- lib/shared/utils/const.dart
+++ lib/shared/utils/const.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/material.dart' show kToolbarHeight;
+import 'package:flutter/material.dart'
+    show kBottomNavigationBarHeight, kToolbarHeight;
 import 'package:get/get.dart' show Get, GetNavigation;
 
 final double bAppBar = kToolbarHeight + Get.mediaQuery.padding.top;
+final double bNavBar =
+    kBottomNavigationBarHeight + Get.mediaQuery.padding.bottom;
